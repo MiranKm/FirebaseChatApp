81e192435e3adcff4e4531e39989d2ea4201910e
MiranPC <miran9191@gmail.com>
2019-06-22 22:18:14 +0300

    adds recyclerview adapter to the project

--- app/src/main/java/project/miran/com/firebasechatapp/MessageAdapter.kt
+++ app/src/main/java/project/miran/com/firebasechatapp/MessageAdapter.kt
@@ -1,22 +1,37 @@
 package project.miran.com.firebasechatapp
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_message.view.*
 
-class MessageAdapter (var list:MutableList<FriendlyMessage>): RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+class MessageAdapter(var list: MutableList<FriendlyMessage>) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val inflater = LayoutInflater.from(parent.context).inflate(R.layout.item_message, parent, false)
+        return ViewHolder(inflater)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return list.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.setData(list.get(position))
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val photoView = itemView.photoImageView
+        val messageTV = itemView.messageTextView
+        val authorTV = itemView.nameTextView
+
+
+        fun setData(data: FriendlyMessage) {
+            Glide.with(photoView.context).load(data.imageUrl).into(photoView)
+            messageTV.text = data.text
+            authorTV.text = data.name
+        }
+    }
 }
